153f3be0900bbe34f026040204e6352de4acfd62
rohank <Rohan.Kamat@ellucian.com>
2021-01-27 22:34:24 +0530

    forcast changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,15 @@
 const request = require('request')
 
-
 const forecast = (latitude, longitude, callback) => {
-    //units = m
-    //units = f
-    //units = k
-    const url = 'http://api.weatherstack.com/current?access_key=98842ec3a2d918e43f70912f7ad8c924&query=' + latitude + ',' + longitude + '&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=98842ec3a2d918e43f70912f7ad8c924f&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
